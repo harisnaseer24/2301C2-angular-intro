83367526f64d1238d1b9bc0bc5ea5e15b858bbc2
asp <asp@aptech.com>
2024-10-28 12:41:50 +0500

    routing and data-binding one way and two way

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutSectionComponent } from './about-section/about-section.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:"",
+        component:HomeComponent
+    },
+    {
+        path:"about",
+        component:AboutSectionComponent
+    }
+];
